03c7e528dc37fdc7ab4c6fb7d26abd9d4efe706b
Lars Rosenström <lars.rosenstrom@kb.se>
2021-05-18 11:38:12 +0200

    Handle plugin activation/deactivation at the site level

--- idkbse/IdkbsePlugin.inc.php
+++ idkbse/IdkbsePlugin.inc.php
@@ -30,6 +30,20 @@ class IdkbsePlugin extends GenericPlugin {
         return "Integrates swedish subject headings from id.kb.se with the keyword field.";
     }
 
+    function isSitePlugin() {
+		return true;
+	}
+
+	// Can only be activated at site level.
+	function getCanEnable() {
+		return !((bool) Application::get()->getRequest()->getContext());
+	}
+
+	// Can only be deactivated at site level.
+	function getCanDisable() {
+		return !((bool) Application::get()->getRequest()->getContext());
+	}
+
     function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path);
 		if ($success && $this->getEnabled()) {
